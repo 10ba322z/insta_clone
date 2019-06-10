e90be322d6af7feb19e3abecc635d2ea946a8711
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-06-10 16:47:04 +0900

    add some user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :firstname, presence: true
+  validates :lastname,  presence: true
+  validates :username,  presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email,     presence: true,  format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
 end
